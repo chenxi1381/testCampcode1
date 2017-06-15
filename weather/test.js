ee437dd62a00fe3ef7df216cd0b9d8f12dc1fc74
chenxi1381 <chenxi1381@163.com>
2017-06-15 21:35:36 +0800

    更改js 写的方式

--- weather/test.js
+++ weather/test.js
@@ -1,17 +1,11 @@
-function finishLesson(lesson) {
-
-	alert("恭喜，你已经完成HBuilder入门课程。你可以用其它开发工具试试写这几十行代码，至少比HBuilder慢5倍！更重要的是，你很难找到这么爽的编码体验。");
-}
-
-function getInfo() {
-
-	$.ajax({
-		url: "http://v.juhe.cn/weather/index?format=2&cityname=%E8%8B%8F%E5%B7%9E&key=719eb318cac14d55e0a3b20204646700",
-		type: 'GET',
-		dataType: 'JSONP', //here
-		success: function(data) {
-			console.log(data);
-
+var minFunction = {
+	Bind: function(){
+		$('#request_weather').click(function() {
+			minFunction.getData($(this).attr("class"));
+		});
+	},
+	getData: function(fromName){
+		CallAjax("http://v.juhe.cn/weather/index?format=2&cityname=%E8%8B%8F%E5%B7%9E&key=719eb318cac14d55e0a3b20204646700",function(data){
 			var line = $("#row_list").clone();
 			var result = data.result.today;
 
@@ -24,12 +18,34 @@ function getInfo() {
 			line.find('#row_dressing_index').text(result.dressing_index);
 			line.find('#row_dressing_advice').text(result.dressing_advice);
 			line.appendTo("#row_box");
+			
+			
+//			console.log(fromName);
+//			console.log(result.data.abc[0]);
+//			$("." + fromName).html("" + result.data.abc[0].huan + "");
+		});
+	},
+	iniTiation: function(){
+		this.Bind();
+	}
+}
+
+$(function(){
+	minFunction.iniTiation();
+});
+//
+function CallAjax(url,callBack) {
+	$.ajax({
+		type:"get",
+		dataType: 'JSONP', //here
+		url:url,
+		success:function(result){
+			console.log("success:" +result);
+			return callBack(result);
+		},
+		error:function(e){
+			console.log(e);
 		}
 	});
 }
-$(document).ready(function() {
-	//	getInfo();
-	$('#request_weather').click(function() {
-		getInfo();
-	});
-});
+//
